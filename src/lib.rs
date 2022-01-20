aab718416a8fe295df0bc950a630784a2b17aae5
Keith Valin <keith@mailtux.net>
2022-01-20 16:04:37 -0500

    Converted indentation to tabs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use libc::c_void;
 type Account = *mut c_void;
 
 extern "C" {
-    fn create_account() -> Account;
-    fn print(account: Account);
-    fn destroy_account(account: Account);
+	fn create_account() -> Account;
+	fn print(account: Account);
+	fn destroy_account(account: Account);
 }
