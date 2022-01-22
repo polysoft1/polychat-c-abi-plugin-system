3f8f141cf02bc85bb09b8df6e1f13e856887197c
Keith Valin <keith@mailtux.net>
2022-01-21 21:09:59 -0500

    Added option to skip cbind generation, prevents package issues

--- build.rs
+++ build.rs
@@ -3,6 +3,9 @@ extern crate cbindgen;
 use std::env;
 
 fn main() {
+	if env::var("SKIP_CBINDGEN").unwrap_or("0".to_string()) == "1" {
+		return;
+	}
 	let out_dir = env::var("OUT_DIR").unwrap();
 	let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 	let package_name = env::var("CARGO_PKG_NAME").unwrap().replace("-", "_");
